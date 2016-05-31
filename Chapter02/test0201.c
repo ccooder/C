64aafcee7a461820ea65e628be884beca270c7c8
C++ <niufenglu@gmail.com>
2016-05-31 21:36:25 +0800

    Chapter02

---
+++ Chapter02/test0201.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz0 = 0;
+int abcdefghijklmnopqrstuvwxyz1 = 1;
+int main()
+{
+    printf("%d",abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz0);
+}
